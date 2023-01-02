c8d9ee6a5e5187b999bb5339ba66a25adee190fc
Charlotte333333 <112644693+Charlotte333333@users.noreply.github.com>
2023-01-02 15:04:17 -0500

    hh

--- assignment_five.py
+++ assignment_five.py
@@ -2,10 +2,12 @@
 import random
 
 def get_number():
+    """This function sets the random number for the user to guess"""
     number = random.randint(1,100)
     return number
 
 def introduction():
+    """This function introduces the game and gets the users name"""
     print("Hi queen! This is a guessing game. You guess a number between 1-100 and ill tell you if its too high or too low, until you get it right.")
     user_name = input("What is your name?")
     return user_name
@@ -16,10 +18,12 @@ def introduction():
 
 
 def get_guess():
+    """This function gets the user to enter their guess"""
     guess1 = input("Enter your guess")
     return guess1
 
 def main():
+    """This function plays the game"""
     introduction()
     for x in range(3):
         print("New game")
